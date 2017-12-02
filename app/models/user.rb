30b87459d5ed6730e9641667b7c7589cef88c50d
Julia Dahm <jukabrown@gmail.com>
2017-12-01 20:17:32 -0500

    User controller crud actions built out

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
   attr_accessible :username, :questions_attributes
   has_many :questions
   accepts_nested_attributes_for :questions, allow_destroy: true
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 end

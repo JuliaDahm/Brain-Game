dc8654fa49227835a08323cf41701fad7e47c05b
Julia Dahm <jukabrown@gmail.com>
2017-12-03 22:29:26 -0500

    Added and configured Omniauth gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,23 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :confirmable, :lockable, :timeoutable and
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
-  attr_accessible :username, :questions_attributes
+  :recoverable, :rememberable, :trackable, :validatable,
+  :omniauthable, :omniauth_providers => [:facebook]
   has_many :questions
   accepts_nested_attributes_for :questions, allow_destroy: true
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+
+      if user.provider == "facebook"
+        user.email = "#{auth.info.nickname}@facebook.com"
+      else
+        user.email = auth.info.email
+      end
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
 end
